59cf1a67b5b99d69c785e579faa648f67aec3b2b
neavo <neavo@neavo.me>
2025-04-04 00:21:34 +0800

    修正 - 标点修复规则未生效的问题

--- module/Fixer/PunctuationFixer.py
+++ module/Fixer/PunctuationFixer.py
@@ -4,47 +4,14 @@ from base.BaseLanguage import BaseLanguage
 
 class PunctuationFixer():
 
-    # 数量一致才执行的规则 - A区
-    RULE_SAME_COUNT_A = {
-        "　": (" ", ),                                      # 全角空格和半角空格之间的转换
-        "：": (":", ),
-        "・": ("·", ),
-        "？": ("?", ),
-        "！": ("!", ),
-        "\u2014": ("\u002d", "\u2015"),                     # 破折号之间的转换，\u002d = - ，\u2014 = ― ，\u2015 = —
-        "\u2015": ("\u002d", "\u2014"),                     # 破折号之间的转换，\u002d = - ，\u2014 = ― ，\u2015 = —
-        "＜": ("<", "《"),
-        "＞": (">", "》"),
-        "「": ("‘", "“", "『"),
-        "」": ("’", "”", "』"),
-        "『": ("‘", "“", "「"),
-        "』": ("’", "”", "」"),
-        "（": ("(", "「", "‘", "“"),
-        "）": (")", "」", "’", "”"),
-    }
-
-    # 数量一致才执行的规则 - B区
-    RULE_SAME_COUNT_B = {
-        " ": ("　", ),                                      # 全角空格和半角空格之间的转换
-        ":": ("：", ),
-        "·": ("・", ),
-        "?": ("？", ),
-        "!": ("！", ),
-        "\u002d": ("\u2014", "\u2015"),                     # 破折号之间的转换，\u002d = - ，\u2014 = ― ，\u2015 = —
-        "<": ("＜", "《"),
-        ">": ("＞", "》"),
-        "(": ("（", "「", "‘", "“"),
-        ")": ("）", "」", "’", "”"),
-    }
-
     # 正则规则
-    RULE_REGEX_CJK = [
+    RULE_REGEX: list[dict] = [
         {
             "source_language_tuple" : None,
             "target_language_tuple" : None,
             "regex_src": re.compile(r"^「.*」$", flags = re.IGNORECASE),
             "regex_dst": None,
-            "regex_repl": re.compile(r"^\"(.*)\"$", flags = re.IGNORECASE),
+            "regex_repl": re.compile(r"^['\"‘“](.*)['\"’”]$", flags = re.IGNORECASE),
             "repl": r"「\1」",
         },
         {
@@ -52,31 +19,73 @@ class PunctuationFixer():
             "target_language_tuple" : None,
             "regex_src": re.compile(r"^『.*』$", flags = re.IGNORECASE),
             "regex_dst": None,
-            "regex_repl": re.compile(r"^\"(.*)\"$", flags = re.IGNORECASE),
+            "regex_repl": re.compile(r"^['\"‘“](.*)['\"’”]$", flags = re.IGNORECASE),
             "repl": r"『\1』",
-        },
-        {
-            "source_language_tuple" : None,
-            "target_language_tuple" : (BaseLanguage.ZH, BaseLanguage.JA, BaseLanguage.KO),
-            "regex_src": re.compile(r"^“.*”$", flags = re.IGNORECASE),
-            "regex_dst": None,
-            "regex_repl": re.compile(r"^\"(.*)\"$", flags = re.IGNORECASE),
-            "repl": r"“\1”",
         },
         {
             "source_language_tuple" : None,
             "target_language_tuple" : (BaseLanguage.ZH, BaseLanguage.JA, BaseLanguage.KO),
             "regex_src": re.compile(r"^‘.*’$", flags = re.IGNORECASE),
             "regex_dst": None,
-            "regex_repl": re.compile(r"^'(.*)'$", flags = re.IGNORECASE),
+            "regex_repl": re.compile(r"^['\"‘“](.*)['\"’”]$", flags = re.IGNORECASE),
             "repl": r"‘\1’",
-        }
+        },
+        {
+            "source_language_tuple" : None,
+            "target_language_tuple" : (BaseLanguage.ZH, BaseLanguage.JA, BaseLanguage.KO),
+            "regex_src": re.compile(r"^“.*”$", flags = re.IGNORECASE),
+            "regex_dst": None,
+            "regex_repl": re.compile(r"^['\"‘“](.*)['\"’”]$", flags = re.IGNORECASE),
+            "repl": r"“\1”",
+        },
     ]
 
-    # 强制规则
-    RULE_FORCE_CJK = {
-        "「": ("‘", "“"),
-        "」": ("’", "”"),
+    # 数量匹配规则
+    RULE_SAME_COUNT_A: dict[str, tuple[str]] = {
+        "　": (" ", ),                                      # 全角空格和半角空格之间的转换
+        "：": (":", ),
+        "・": ("·", ),
+        "？": ("?", ),
+        "！": ("!", ),
+        "\u2014": ("\u002d", "\u2015"),                     # 破折号之间的转换，\u002d = - ，\u2014 = ― ，\u2015 = —
+        "\u2015": ("\u002d", "\u2014"),                     # 破折号之间的转换，\u002d = - ，\u2014 = ― ，\u2015 = —
+        "<": ("＜", "《"),
+        ">": ("＞", "》"),
+        "＜": ("<", "《"),
+        "＞": (">", "》"),
+        "[": ("【", ),
+        "]": ("】", ),
+        "【": ("[", ),
+        "】": ("]", ),
+        "(": ("（", ),
+        ")": ("）", ),
+        "（": ("(", ),
+        "）": (")", ),
+        "「": ("‘", "“", "『"),
+        "」": ("’", "”", "』"),
+        "『": ("‘", "“", "「"),
+        "』": ("’", "”", "」"),
+        "‘": ("“", "「", "『"),
+        "’": ("”", "」", "』"),
+        "“": ("‘", "「", "『"),
+        "”": ("’", "」", "』"),
+    }
+
+    # 数量匹配规则
+    RULE_SAME_COUNT_B: dict[str, tuple[str]] = {
+        " ": ("　", ),                                      # 全角空格和半角空格之间的转换
+        ":": ("：", ),
+        "·": ("・", ),
+        "?": ("？", ),
+        "!": ("！", ),
+        "\u002d": ("\u2014", "\u2015"),                     # 破折号之间的转换，\u002d = - ，\u2014 = ― ，\u2015 = —
+    }
+
+    # 强制替换规则
+    # 译文语言为 CJK 语言时，执行此规则
+    RULE_FORCE_CJK: dict[str, tuple[str]] = {
+        "「": ("“"),
+        "」": ("”"),
     }
 
     def __init__(self) -> None:
@@ -86,7 +95,7 @@ class PunctuationFixer():
     @classmethod
     def fix(cls, src: str, dst: str, source_language: str, target_language: str) -> str:
         # 执行正则区规则
-        for rule in cls.RULE_REGEX_CJK:
+        for rule in cls.RULE_REGEX:
             regex_src: re.Pattern = rule.get("regex_src")
             regex_dst: re.Pattern = rule.get("regex_dst")
             source_language_tuple: tuple[str] = rule.get("source_language_tuple")
@@ -97,25 +106,27 @@ class PunctuationFixer():
                 continue
             if isinstance(regex_dst, re.Pattern) and regex_dst.search(dst) == None:
                 continue
-            if isinstance(source_language_tuple, tuple) and not source_language in source_language_tuple:
+            if isinstance(source_language_tuple, tuple) and source_language not in source_language_tuple:
                 continue
-            if isinstance(target_language_tuple, tuple) and not target_language in target_language_tuple:
+            if isinstance(target_language_tuple, tuple) and target_language not in target_language_tuple:
                 continue
 
             dst = rule.get("regex_repl").sub(rule.get("repl"), dst)
 
         # CJK To CJK = A + B
-        # CJK To 非CJK = B
+        # CJK To 非CJK = A + B
         # 非CJK To CJK = A
-        # 非CJK To 非CJK = B
+        # 非CJK To 非CJK = A + B
         if BaseLanguage.is_cjk(source_language) and BaseLanguage.is_cjk(target_language):
             cls.apply_fix_rules(src, dst, cls.RULE_SAME_COUNT_A)
             cls.apply_fix_rules(src, dst, cls.RULE_SAME_COUNT_B)
         elif BaseLanguage.is_cjk(source_language) and not BaseLanguage.is_cjk(target_language):
+            cls.apply_fix_rules(src, dst, cls.RULE_SAME_COUNT_A)
             cls.apply_fix_rules(src, dst, cls.RULE_SAME_COUNT_B)
         elif not BaseLanguage.is_cjk(source_language) and BaseLanguage.is_cjk(target_language):
             cls.apply_fix_rules(src, dst, cls.RULE_SAME_COUNT_A)
         else:
+            cls.apply_fix_rules(src, dst, cls.RULE_SAME_COUNT_A)
             cls.apply_fix_rules(src, dst, cls.RULE_SAME_COUNT_B)
 
         # 译文语言为 CJK 语言时，执行强制规则
@@ -145,6 +156,7 @@ class PunctuationFixer():
         for key, value in rules.items():
             if cls.check(src, dst, key, value) == True:
                 dst = cls.apply_replace_rules(dst, key, value)
+
         return dst
 
     # 应用替换规则
