76bf7fec009314d3601b2c2eea330eff5a05e612
neavo <neavo@neavo.me>
2025-05-06 23:07:39 +0800

    调整 - 更新打包步骤

--- resource/pyinstaller.py
+++ resource/pyinstaller.py
@@ -5,10 +5,11 @@ cmd = [
     "./app.py",
     "--icon=./resource/icon.ico",
     "--clean", # Clean PyInstaller cache and remove temporary files before building
-    # "--onedir", # Create a one-folder bundle containing an executable (default)
-    "--onefile", # Create a one-file bundled executable
+    "--onedir", # Create a one-folder bundle containing an executable (default)
+    # "--onefile", # Create a one-file bundled executable
     "--noconfirm", # Replace output directory (default: SPECPATH/dist/SPECNAME) without asking for confirmation
-    "--distpath=./dist/LinguaGacha", # Where to put the bundled app (default: ./dist)
+    "--distpath=./dist", # Where to put the bundled app (default: ./dist)
+    "--name=LinguaGacha"
 ]
 
 if os.path.exists("./requirements.txt"):
@@ -17,4 +18,9 @@ if os.path.exists("./requirements.txt"):
             if "#" not in line:
                 cmd.append("--hidden-import=" + line.strip())
 
+    # 执行打包
     PyInstaller.__main__.run(cmd)
+
+    # 更名
+    if os.path.isfile("./dist/LinguaGacha/LinguaGacha.exe"):
+        os.rename("./dist/LinguaGacha/LinguaGacha.exe", "./dist/LinguaGacha/app.exe")
