62b7391b29df0c96dea682e9c9bd3646d2b4a6e2
neavo <neavo@neavo.me>
2025-05-29 12:41:59 +0800

    新增 - T++ 项目文件中对重复文本去重（专家设置）

--- module/Config.py
+++ module/Config.py
@@ -50,6 +50,7 @@ class Config(BaseData):
         # ExpertSettingsPage
         self.preceding_lines_threshold: int = 3
         self.enable_preceding_on_local: bool = False
+        self.deduplication_in_trans: bool = True
         self.deduplication_in_bilingual: bool = True
         self.write_translated_name_fields_to_file: bool = True
         self.result_checker_retry_count_threshold: bool = False
@@ -121,6 +122,7 @@ class Config(BaseData):
         # ExpertSettingsPage
         self.preceding_lines_threshold: int = 3
         self.enable_preceding_on_local: bool = False
+        self.deduplication_in_trans: bool = True
         self.deduplication_in_bilingual: bool = True
         self.write_translated_name_fields_to_file: bool = True
         self.result_checker_retry_count_threshold: bool = False
