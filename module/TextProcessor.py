4dcab884b59781e34424e158aca6039a25cbb4fb
SarContDeli <48789988+SaiCateDoan@users.noreply.github.com>
2025-10-09 20:00:43 +0800

    修复 - 注音规则过于宽泛误伤正常文本

--- module/TextProcessor.py
+++ module/TextProcessor.py
@@ -158,7 +158,7 @@ class TextProcessor(Base):
         if self.config.clean_ruby == False:
             return src
         else:
-            return RubyCleaner.clean(src)
+            return RubyCleaner.clean(src, self.item.get_text_type())
 
     # 自动修复
     def auto_fix(self, src: str, dst: str) -> str:
