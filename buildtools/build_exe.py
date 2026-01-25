42731783985cacee51e61f7b2620fabf9359da72
neavo <neavo@neavo.me>
2026-01-25 12:29:06 +0800

    ci

--- buildtools/build_exe.py
+++ buildtools/build_exe.py
@@ -8,6 +8,16 @@ is_macos = sys.platform == "darwin"
 is_linux = sys.platform == "linux"
 is_windows = sys.platform == "win32" or os.name == "nt"
 
+# 公共配置
+common_args = [
+    "--collect-all=rich",
+]
+
+# 添加资源文件
+sep = ";" if is_windows else ":"
+common_args.append(f"--add-data=version.txt{sep}.")
+common_args.append(f"--add-data=resource{sep}resource")
+
 if is_macos:
     # macOS：创建 .app 应用包
     cmd = [
@@ -20,7 +30,7 @@ if is_macos:
         "--noconfirm",
         "--distpath=./dist",
         "--osx-bundle-identifier=me.neavo.linguagacha",
-    ]
+    ] + common_args
 elif is_linux:
     # Linux：创建用于 AppImage 的目录包
     cmd = [
@@ -30,17 +40,19 @@ elif is_linux:
         "--onedir",
         "--noconfirm",
         "--distpath=./dist",
-    ]
+    ] + common_args
 else:
     # Windows：创建单文件可执行程序
     cmd = [
         "./app.py",
+        "--name=LinguaGacha",
         "--icon=./resource/icon.ico",
         "--clean",
         "--onefile",
         "--noconfirm",
         "--distpath=./dist/LinguaGacha",
-    ]
+    ] + common_args
+
 
 # 从 requirements.txt 添加隐式依赖
 if os.path.exists("./requirements.txt"):
